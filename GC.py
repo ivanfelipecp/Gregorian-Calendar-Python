476d16fe3209e3c6d82bcbb0c95d7f04c056a38e
Iván Calvo Pérez <ivanfelipecp@gmail.com>
2018-03-19 11:05:44 -0600

    Commit

--- GC.py
+++ GC.py
@@ -1,39 +1,45 @@
 class GregorianCalendar():
-    fecha = ()
+    # Variables auxiliares
     diaMeses = [31,28,31,30,31,30,31,31,30,31,30,31]
+    dias = ["domingo","lunes","martes","miercoles","jueves","viernes","sabado"]
     a = 0
     m = 1
     d = 2
     error = "error con la fecha"
 
-    def validarBisiesto(self,a):
-        flag = (a%4==0) and (a%400==0)
+    # Verifica si un año es bisiesto
+    def bisiesto(self,a):
+        if type(a)!= int:
+            return False
+        flag = (a%4==0 and a%100!=0) or (a%100==4)
         if flag:
             self.diaMeses[1] = 29
         return flag
+
+    # Rango de años para utilizar la formula
     def validarRangoA(self,a):
         return a >= 1700 and a <= 2199
-    
-    def validarA(self,a):
-        flag = False
-        if type(a) != int:
-            return flag
 
-        self.validarBisiesto(a)
+    # Valida que sea el año sea un tipo válido y en el rango
+    def validarA(self,a):
+        self.bisiesto(a)
         flag = True
         if not self.validarRangoA(a):
             flag = False
 
         return flag
 
+    # Valida el mes
     def validarM(self,m):
         return (m >= 1) and (m<=12)
 
+    # Valida el día
     def validarD(self,m,d):
         cant = self.diaMeses[m-1]
         return (d>=1) and (d<=cant)
 
-    def validarFechaTupla(self,fecha):
+    # Verifica que la fecha sea tupla de 3 numeros enteros, con datos validos
+    def fecha_es_tupla(self,fecha):
         # Verifica que sea tupla
         flag = type(fecha) == tuple
         if not flag:
@@ -44,16 +50,17 @@ class GregorianCalendar():
         if not flag:
             return flag
 
-        # Verifica que todos sean úmeros
+        # Verifica que todos sean números
         for i in fecha:
             if type(i) != int:
                 flag = False
                 break
         return flag
-        
-    def validarFecha(self, fecha):
+
+    # Usando todas las funciones anteriores de validación, verifica que la fecha sea válida        
+    def fecha_es_valida(self, fecha):
         # Bandera
-        flag = self.validarFechaTupla(fecha)
+        flag = self.fecha_es_tupla(fecha)
         if not flag:
             return flag
 
@@ -63,6 +70,7 @@ class GregorianCalendar():
         flag = flag and self.validarD(fecha[self.m],fecha[self.d])
         return flag
 
+    # Retorna el código del century
     def getCenturyCode(self,a):
         if a>=1700 and a<=1799:
             return 4
@@ -78,20 +86,24 @@ class GregorianCalendar():
             return 2
         else:
             return 0
-        
+
+    # Función la cual retorna el día de la semana a partir de una fecha
+    # Emplea la funcion getCentury y una fórmula
     def getDiaSemana(self,fecha):
-        if self.validarFecha(fecha):
+        if self.fecha_es_valida(fecha):
             y_c = (fecha[self.a] + (fecha[self.a]//4))%7
             m_c = [0,3,3,6,1,4,6,2,5,0,3,5][fecha[self.m]-1]
             c_c = self.getCenturyCode(fecha[self.a])
             l_c = 0
-            if self.validarBisiesto(fecha[self.a]):
+            if self.bisiesto(fecha[self.a]):
                 l_c = 1
             return (y_c + m_c + c_c + fecha[self.d] - l_c)%7 - 1
         else:
             return self.error
-    def diaSiguiente(self,fecha):
-        if self.validarFecha(fecha):
+
+    # Función que calcula el día siguiente, retorna una tupla
+    def dia_siguiente(self,fecha):
+        if self.fecha_es_valida(fecha):
             a = fecha[self.a]
             m = fecha[self.m]
             d = fecha[self.d]
@@ -107,6 +119,7 @@ class GregorianCalendar():
         else:
             return self.error
 
+    # Función auxiliar que valida si f2 es mayor que f1
     def esMayor(self,f1,f2):
         bandera = True
         if f1[self.a] > f2[self.a]:
@@ -116,13 +129,15 @@ class GregorianCalendar():
         elif f1[self.d] > f2[self.d]:
             bandera = False
         return bandera
+    
+    # Calcula la diferencia de días
     def diferenciaDias(self, f1, f2):
-        if self.validarFecha(f1) and self.validarFecha(f2):
+        if self.fecha_es_valida(f1) and self.fecha_es_valida(f2):
             if self.esMayor(f1,f2):
                 f = f1
                 cont = 0
                 while f != f2:
-                    f = a.diaSiguiente(f)
+                    f = self.dia_siguiente(f)
                     cont += 1
                 if f2[self.a] > f1[self.a]:
                     cont -= f2[self.a] - f1[self.a] - 1
@@ -131,29 +146,43 @@ class GregorianCalendar():
                 return self.error
         else:
             return self.error
-    def diasDesdePrimeroEnero(self,fecha):
+
+    # Hace el llamado de la función diferencia de dias, pero antes crea una fecha en 1 de enero
+    def dias_desde_primero_enero(self,fecha):
         f1 = (fecha[self.a],1,1)
         return self.diferenciaDias(f1,fecha)
-    def diaPrimeroEnero(self,a):
-        return self.getDiaSemana((a,1,1))
+
+    # Usa la función getDiaSemana con el primero de enero
+    def dia_primero_enero(self,a):
+        #print(type(a))
+        if type(a) != int or not self.validarA(a):
+            return self.error
+        return self.dia(self.getDiaSemana((a,1,1)))
+
+    # Función la cual recibe un n entre 0~6 y retorna su respectivo dia, domingo 0... lunes 1...
     def dia(self,n):
-        dias = ["domingo","lunes","martes","miercoles","jueves","viernes","sabado"]
-        return dias[n]
+        
+        return self.dias[n]
+
+    # Función aux que incrementa el dia
     def incrementarDia(self,d):
         d += 1
         if d > 6:
             d = 1
         return d
-    def imprimirA(self,a):
-        if not self.validarFecha((1,1,a)):
+
+    # Funcion que imprime el calendario, usando strings, ciclos y listas
+    def imprimir_3x4(self,a):
+        if not self.fecha_es_valida((1,1,a)):
             return self.error
-        self.validarBisiesto(a)
+        self.bisiesto(a)
         meses1 = "{0}Enero{0} |{0}Febrero{1} |{0} Marzo{0} |{0} Abril{0} |".format(" "*11,10*" ")
         meses2 = "{0} Mayo {0}  |{0} Julio {0}  |{0} Julio {0}  |{0} Agosto {0} |".format(" "*10)
         meses3 = "{0} Setiembre  {0}|{0} Octubre {0}    |{0} Noviembre {0}  |{0} Diciembre {0}  |".format(" "*8)
         mesPrint = [meses1,meses2,meses3]
         dias = "D   L   K   M   J   V   S   | D   L   K   M   J   V   S   | D   L   K   M   J   V   S   | D   L   K   M   J   V   S   |"
         
+        # Crea la matriz inicial
         meses = []
         for i in range(12):
             m = []
@@ -161,9 +190,11 @@ class GregorianCalendar():
                 m.append([" "," "," "," "," "," "," "])
             meses.append(m)
         
-        diaI = self.diaPrimeroEnero(a)     
+        # Obtiene el dia donde inicia el año
+        diaI = self.dia_primero_enero(a)     
         mes = 0
 
+        # Asigna el numero de dia a cada mes en cada semana
         while mes < 12:
             cantD = self.diaMeses[mes]
             semana = 0
@@ -186,7 +217,7 @@ class GregorianCalendar():
             if (diaI>6):
                 diaI = 0
 
-        #for i in range(3):
+        # Imprime los primeros 4
         print(mesPrint[0])
         print(dias)
         
@@ -204,9 +235,9 @@ class GregorianCalendar():
                     print(a,end=" ")
                 print("| ",end="")
             print()
-
-
-        #for i in range(3):
+        
+        # Imprime los segundos 4
+        print()
         print(mesPrint[1])
         print(dias)
         
@@ -225,7 +256,8 @@ class GregorianCalendar():
                 print("| ",end="")
             print()
 
-        #for i in range(3):
+        # Imprime los últimos cuatro
+        print()
         print(mesPrint[2])
         print(dias)
         
@@ -243,15 +275,4 @@ class GregorianCalendar():
                     print(a,end=" ")
                 print("| ",end="")
             print()
-        
-            
-            
-        
-        
-a = GregorianCalendar()
-a.imprimirA(2018)
-
-#f1 = (2018,11,28)
-#print(a.diasDesdePrimeroEnero(f1))
-
-#print(a.imprimirA(2018))
+        print()
---
+++ pruebas.py
@@ -0,0 +1,44 @@
+from GC import *
+
+gc = GregorianCalendar()
+
+fecha1 = (2016,2,29)
+fecha2 = (2018,8,12)
+fecha3 = (2019,21,50)
+fecha4 = (1,1,1)
+fecha5 = ("a","b",True)
+fecha6 = [2018,1,1]
+fechas = [fecha1,fecha2,fecha3,fecha4,fecha5,fecha6]
+
+
+
+print("Pruebas de GC")
+print("Se realizarán las pruebas con los siguientes datos:")
+for i in fechas:
+    print(i)
+
+print("De los cuales, solo los dos primeros son correctos")
+
+print("\n Prueba de R0: fecha es tupla")
+for i in fechas:
+    print(i,"-> ",gc.fecha_es_tupla(i))
+
+print("\n Prueba de R1: Bisiesto")
+for i in fechas:
+    print(i[0],"-> ",gc.bisiesto(i[0]))
+
+print("\n Prueba de R2: Fecha es válida")
+for i in fechas:
+    print(i,"-> ",gc.fecha_es_valida(i))
+
+print("\n Prueba de R3: Día siguiente")
+for i in fechas:
+    print(i,"-> ",gc.dia_siguiente(i))
+
+print("\n Prueba de R4: Dias desde primero enero")
+for i in fechas:
+    print(i,"-> ",gc.dias_desde_primero_enero(i))
+
+print("\n Prueba de R5: Día primero enero")
+for i in fechas:
+    print(i[0],"-> ",gc.dia_primero_enero(i[0]))
